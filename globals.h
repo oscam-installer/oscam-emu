5b7d2a15c7b7f6d59be8c31fc35d38a6bbadf31b
oscam-installer <oscam-installer@proton.me>
2025-04-25 06:29:22 +0200

    cmake: fix build with version 4.0.0
        * update copyright year
        * css fix white space and tab
    
        thx @heitbaum for cmake fix

--- globals.h
+++ globals.h
@@ -372,11 +372,7 @@
 #define WIKI_URL				"https://wiki.streamboard.tv/wiki"
 #define BOARD_URL				"https://board.streamboard.tv"
 #ifndef CS_VERSION
-<<<<<<< HEAD
-#define CS_VERSION				"2.25.04-11879"
-=======
-#define CS_VERSION				"2.25.04-11880"
->>>>>>> 208972e6fe881b2cd7918101c8704f64b08d5341
+#define CS_VERSION				"2.25.04-11881"
 #endif
 #ifndef CS_GIT_COMMIT
 #define CS_GIT_COMMIT			"a2b4c6d8"
